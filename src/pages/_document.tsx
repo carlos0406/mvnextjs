ea1a1247f49f50142c805a0933fe44d76dfa804f
Carlos Henrique <54608090+carlos0406@users.noreply.github.com>
2021-06-16 11:48:19 -0300

    #00:setup rules

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,18 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
@@ -21,16 +20,15 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      };
+        )
+      }
     } finally {
-      sheet.seal();
+      sheet.seal()
     }
   }
 
   render() {
     return (
-
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
@@ -45,6 +43,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
