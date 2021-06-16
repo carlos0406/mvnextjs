b331f47eaff2891e48b29548e4e25ec4fbb9925c
Carlos Henrique <54608090+carlos0406@users.noreply.github.com>
2021-06-16 14:32:59 -0300

    @01:add styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import GlobalStyle from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/themes/theme'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
